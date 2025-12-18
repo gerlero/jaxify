971fa36a66e93875939fa22a3ecfc09c899ad0ff
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-12-18 00:25:15 -0300

    Support logical and chained comparison constructs

--- tests/test_unsupported.py
+++ tests/test_unsupported.py
@@ -1,10 +1,11 @@
-import jax
+from collections.abc import AsyncGenerator, Generator
+
 import pytest
-from jaxify import jaxify
+from jaxify import JaxifyError, jaxify
 
 
 def test_for_loop() -> None:
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(JaxifyError):
 
         @jaxify
         def sum_up_to_n(n: int) -> int:
@@ -15,7 +16,7 @@ def test_for_loop() -> None:
 
 
 def test_while_loop() -> None:
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(JaxifyError):
 
         @jaxify
         def countdown(n: int) -> int:
@@ -24,71 +25,24 @@ def test_while_loop() -> None:
             return n
 
 
-def test_and_jit() -> None:
-    @jax.jit
-    @jaxify
-    def logical_and(a: bool, b: bool, /) -> bool:  # noqa: FBT001
-        return a and b
+def test_generator() -> None:
+    with pytest.raises(TypeError):
 
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_and(True, False) is False  # noqa: FBT003
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_and(True, True) is True  # noqa: FBT003
+        @jaxify
+        def generator(n: int) -> Generator[int]:
+            yield from range(n)
 
 
-def test_or_jit() -> None:
-    @jax.jit
-    @jaxify
-    def logical_or(a: bool, b: bool, /) -> bool:  # noqa: FBT001
-        return a or b
+def test_async() -> None:
+    with pytest.raises(TypeError):
 
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_or(False, False) is False  # noqa: FBT003
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_or(True, False) is True  # noqa: FBT003
+        @jaxify
+        async def coroutine(x: int) -> int:
+            return x + 1
 
+    with pytest.raises(TypeError):
 
-def test_and_or_jit() -> None:
-    @jax.jit
-    @jaxify
-    def logical_and_or(a: bool, b: bool, c: bool, /) -> bool:  # noqa: FBT001
-        return (a and b) or c
-
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_and_or(True, False, False) is False  # noqa: FBT003
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_and_or(True, True, False) is True  # noqa: FBT003
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert logical_and_or(False, False, True) is True  # noqa: FBT003
-
-
-def test_inequality_jit() -> None:
-    @jax.jit
-    @jaxify
-    def check_inequality(x: int, y: int, /) -> bool:
-        return (x > 0 and y < 0) or (x < 0 and y > 0)
-
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_inequality(1, -1) is True
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_inequality(-1, 1) is True
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_inequality(1, 1) is False
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_inequality(-1, -1) is False
-
-
-def test_chained_inequalities_jit() -> None:
-    @jax.jit
-    @jaxify
-    def check_chained_inequalities(x: int, y: int, z: int, /) -> bool:
-        return 0 < x < 10 and -10 < y < 0 and z == 5
-
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_chained_inequalities(5, -5, 5) is True
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_chained_inequalities(0, -5, 5) is False
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_chained_inequalities(5, 0, 5) is False
-    with pytest.raises(Exception, match="Attempted boolean conversion"):
-        assert check_chained_inequalities(5, -5, 0) is False
+        @jaxify
+        async def async_generator(x: int) -> AsyncGenerator[int]:
+            for i in range(x):
+                yield i
